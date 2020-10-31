8c9edc2fb2a014ed843f24b2570d1a29dc49a65e
Gorm Labenz <gormlabenz@gorms-mbp.localdomain>
2020-10-31 21:54:23 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <p class=" text-6xl">Hello You</p>
-    <HelloWorld class="" msg="Welcome to Your Vue.js App" />
-    <div
-      class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ..."
-    >
-      Test
+  <div class="w-screen h-screen flex items-center py-48">
+    <div class="flex flex-col justify-between w-1/4 h-64 min-h-full mx-16 p-4 ">
+      <div class="overflow-y-auto w-full mb-4">
+        <div
+          v-for="message in messages"
+          :key="message.time"
+          class="flex"
+          v-bind:class="{
+            'flex-row-reverse': message.re,
+          }"
+        >
+          <p
+            class="text-white px-4 py-2  bg-blue-900 shadow-lg "
+            v-bind:class="{
+              'rounded-full rounded-tr-none self-end': message.re,
+              'rounded-full rounded-tl-none ': !message.re,
+            }"
+          >
+            {{ message.text }}
+          </p>
+          <div class="w-full h-full"></div>
+        </div>
+      </div>
+      <div class="self-end w-full flex justify-between space-x-2">
+        <input
+          v-model="activeMessage"
+          placeholder="Send message"
+          type="text"
+          class="bg-gray-900 w-full shadow-lg rounded-full px-4 py-2 text-white"
+        />
+        <button
+          @click="sendMessage"
+          class="bg-red-500 px-4 py-2 rounded-full font-bold text-white shadow-lg outline-none appearance-none"
+        >
+          Send
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      activeMessage: "",
+      messages: [],
+    };
+  },
+  methods: {
+    sendMessage() {
+      let remainder = false;
+      if (this.messages.length % 2 == 1) {
+        remainder = true;
+      }
+      if (this.activeMessage) {
+        this.messages.push({
+          user: true,
+          text: this.activeMessage,
+          time: new Date().toTimeString(),
+          re: remainder,
+        });
+        this.activeMessage = "";
+      }
+    },
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
